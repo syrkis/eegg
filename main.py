d2d468f62d4148a26324f00b42e45fd8136a8da8
Noah Syrkis <noah@syrkis.com>
2025-01-01 18:59:29 +0100

    update deps and make ppo work with env.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# main.py
+# %% main.py
 #   eegg
 # by: Noah Syrkis
 # from stable_baselines3 import PPO
 # from stable_baselines3.common import logger
 from ollama import chat, ChatResponse, Message
+from stable_baselines3 import PPO
+from stable_baselines3.common import logger
 import numpy as np
 import base64
 import os
 from PIL import Image
+from craftium.wrappers import DiscreteActionWrapper
 import io
 
 # import gymnasium as gym
@@ -41,10 +44,10 @@ def god_fn(
 def ova_fn(
     observation, reward, terminated, truncated
 ) -> int:  # three leter word for agent inside an egg (also name for collection of cells in an egg)
-    return 0
+    return 0  # Action and words to god
 
 
-def egg_fn(env_name):  # the egg in which the agent is in parrented by god
+def egg_fn(env_name):  # the egg in which the agent is in parented by god
     path = os.path.join(os.path.dirname(craftium.__file__), "craftium-envs", env_name)
     env = CraftiumEnv(
         env_dir=path,  # type: ignore
@@ -52,22 +55,43 @@ def egg_fn(env_name):  # the egg in which the agent is in parrented by god
         obs_width=64,
         obs_height=64,
     )
-    observation, info = env.reset()
-    messages = []
-    reward, terminated, truncated = 0, False, False
-    action = env.action_space.sample()
-    print(action)
-    exit()
 
-    for t in range(10):
-        action = ova_fn(messages, observation, reward, terminated, truncated)
-        observation, reward, terminated, truncated, _info = env.step(action)
-        messages = god_fn(messages, observation, reward, terminated, truncated)
+    # Wrap the environment to simplify the action space
+    env = DiscreteActionWrapper(
+        env,
+        actions=[
+            "forward",
+            "mouse x+",
+            "mouse x-",
+        ],  # Specify the actions you want to use
+        mouse_mov=0.5,
+    )
 
-        if terminated or truncated:
-            observation, info = env.reset()
+    model = PPO("CnnPolicy", env, verbose=1)
+    new_logger = logger.configure("logs-ppo-agent", ["stdout", "csv"])
+    model.set_logger(new_logger)
+    model.learn(total_timesteps=100)
 
     env.close()
 
 
 egg_fn("chop-tree")
+
+
+#
+# observation, info = env.reset()
+# messages = []
+# reward, terminated, truncated = 0, False, False
+# action = env.action_space.sample()
+# print(action)
+# exit()
+#
+# for t in range(10):
+# action = ova_fn(messages, observation, reward, terminated, truncated)
+# observation, reward, terminated, truncated, _info = env.step(action)
+# messages = god_fn(messages, observation, reward, terminated, truncated)
+#
+# if terminated or truncated:
+# observation, info = env.reset()
+#
+# env.close()
